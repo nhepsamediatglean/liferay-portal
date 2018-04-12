879464aa5f0698f068658f7210e6f79fabf6c563
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2018-04-12 17:33:19 +0100

    OAUTH2-124 New oauth2-provider-rest module

---
+++ modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/constants/OAuth2ProviderRestEndpointConstants.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.oauth2.provider.rest.internal.endpoint.constants;
+
+/**
+ * @author Tomas Polesovsky
+ */
+public class OAuth2ProviderRestEndpointConstants {
+
+	public static final String AUTHORIZATION_CODE_PKCE_GRANT =
+		"authorization_code_pkce";
+
+	public static final String CLIENT_REMOTE_ADDR =
+		"CLIENT_REMOTE_ADDR_ADDRESS";
+
+	public static final String CLIENT_REMOTE_HOST = "CLIENT_REMOTE_HOST";
+
+	public static final String COMPANY_ID = "COMPANY_ID";
+
+	public static final String FEATURE_PREFIX = "FEATURE-";
+
+	public static final String FEATURES = "FEATURES";
+
+	public static final String LIFERAY_OAUTH2_CLIENT_ID =
+		"liferay.oauth2.client.id";
+
+	public static final String LIFERAY_OAUTH2_ENDPOINT_PROVIDER =
+		"liferay.oauth2.endpoint.provider";
+
+	public static final String LIFERAY_OAUTH2_ENDPOINT_RESOURCE =
+		"liferay.oauth2.endpoint.resource";
+
+	public static final String TOKEN_INTROSPECTION_FEATURE =
+		"token-introspection";
+
+}
