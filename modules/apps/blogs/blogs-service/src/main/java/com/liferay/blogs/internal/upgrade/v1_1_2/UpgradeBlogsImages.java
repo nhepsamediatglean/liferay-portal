08e1c37db26fd9b1384f06fa51ddf4df709a5b57
Alberto Chaparro <alberto.chaparro@liferay.com>
2019-02-05 21:40:34 +0100

    LPS-88730 SF

--- modules/apps/blogs/blogs-service/src/main/java/com/liferay/blogs/internal/upgrade/v1_1_2/UpgradeBlogsImages.java
+++ modules/apps/blogs/blogs-service/src/main/java/com/liferay/blogs/internal/upgrade/v1_1_2/UpgradeBlogsImages.java
@@ -57,9 +57,9 @@ public class UpgradeBlogsImages extends UpgradeProcess {
 	protected void doUpgrade() throws Exception {
 		try (PreparedStatement ps1 = connection.prepareStatement(
 				SQLTransformer.transform(
-					"select entryId, groupId, companyId, userId, smallImageId " +
-						"from BlogsEntry where smallImage = [$TRUE$] and " +
-							"smallImageId != 0"));
+					"select entryId, groupId, companyId, userId, " +
+						"smallImageId from BlogsEntry where smallImage = " +
+							"[$TRUE$] and smallImageId != 0"));
 			PreparedStatement ps2 = AutoBatchPreparedStatementUtil.autoBatch(
 				connection.prepareStatement(
 					"update BlogsEntry set smallImageFileEntryId = ?, " +
