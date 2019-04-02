943ed7aaa283d21b1e9d8c6df599d2145d5eda7f
Brian Chan <brian.chan@liferay.com>
2019-04-02 15:40:01 -0700

    LPS-91856 By table

--- modules/apps/fragment/fragment-service/src/main/resources/com/liferay/fragment/internal/upgrade/v2_1_0/dependencies/update.sql
+++ modules/apps/fragment/fragment-service/src/main/resources/com/liferay/fragment/internal/upgrade/v2_1_0/dependencies/update.sql
@@ -1,8 +1,12 @@
 alter table FragmentEntry add type_ INTEGER;
-alter table FragmentEntryLink add rendererType INTEGER;
-alter table FragmentEntryLink add rendererKey VARCHAR(75);
 
 COMMIT_TRANSACTION;
 
 update FragmentEntry set type_ = 0;
+
+alter table FragmentEntryLink add rendererKey VARCHAR(75);
+alter table FragmentEntryLink add rendererType INTEGER;
+
+COMMIT_TRANSACTION;
+
 update FragmentEntryLink set rendererType = 0;
