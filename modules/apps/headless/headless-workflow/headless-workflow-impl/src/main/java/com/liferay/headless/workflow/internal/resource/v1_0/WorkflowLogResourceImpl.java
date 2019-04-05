cd0e6c6e0de10b11640755f3dfd584e374d8a424
Preston-Crary <preston.crary@liferay.com>
2019-04-05 10:13:10 -0700

    LPS-93468 Properties have to be set by the AOP.

--- modules/apps/headless/headless-workflow/headless-workflow-impl/src/main/java/com/liferay/headless/workflow/internal/resource/v1_0/WorkflowLogResourceImpl.java
+++ modules/apps/headless/headless-workflow/headless-workflow-impl/src/main/java/com/liferay/headless/workflow/internal/resource/v1_0/WorkflowLogResourceImpl.java
@@ -34,7 +34,6 @@ import org.osgi.service.component.annotations.ServiceScope;
  * @author Javier Gamarra
  */
 @Component(
-	properties = "OSGI-INF/liferay/rest/v1_0/workflow-log.properties",
 	scope = ServiceScope.PROTOTYPE, service = WorkflowLogResource.class
 )
 public class WorkflowLogResourceImpl extends BaseWorkflowLogResourceImpl {
