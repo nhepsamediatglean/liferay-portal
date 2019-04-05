ba36f182d8a75e1e8224e5620e13075d005f6c0a
Preston-Crary <preston.crary@liferay.com>
2019-04-05 10:12:05 -0700

    LPS-93468 Add example usage. After this any methods needing Transactional can have it.

---
+++ modules/apps/headless/headless-workflow/headless-workflow-impl/src/main/java/com/liferay/headless/workflow/internal/resource/v1_0/JAXRSWorkflowLogResource.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.workflow.internal.resource.v1_0;
+
+import com.liferay.headless.workflow.dto.v1_0.WorkflowLog;
+import com.liferay.headless.workflow.resource.v1_0.WorkflowLogResource;
+import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.vulcan.accept.language.AcceptLanguage;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
+
+import javax.annotation.Generated;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+
+/**
+ * @author Preston Crary
+ * @generated
+ */
+@Generated("")
+@Path("/v1.0")
+public interface JAXRSWorkflowLogResource extends WorkflowLogResource {
+
+	@Override
+	@GET
+	@Path("/workflow-logs/{workflowLogId}")
+	@Produces("application/json")
+	@Tags(value = {@Tag(name = "WorkflowLog")})
+	public WorkflowLog getWorkflowLog(
+			@NotNull @PathParam("workflowLogId") Long workflowLogId)
+		throws Exception;
+
+	@Override
+	@GET
+	@Parameters(
+		value = {
+			@Parameter(in = ParameterIn.QUERY, name = "page"),
+			@Parameter(in = ParameterIn.QUERY, name = "pageSize")
+		}
+	)
+	@Path("/workflow-tasks/{workflowTaskId}/workflow-logs")
+	@Produces("application/json")
+	@Tags(value = {@Tag(name = "WorkflowLog")})
+	public Page<WorkflowLog> getWorkflowTaskWorkflowLogsPage(
+			@NotNull @PathParam("workflowTaskId") Long workflowTaskId,
+			@Context Pagination pagination)
+		throws Exception;
+
+	@Context
+	public void setContextAcceptLanguage(
+		AcceptLanguage contextAcceptLanguage);
+
+	@Context
+	public void setContextCompany(Company contextCompany);
+
+	@Context
+	public void setContextUriInfo(UriInfo contextUriInfo);
+
+}
