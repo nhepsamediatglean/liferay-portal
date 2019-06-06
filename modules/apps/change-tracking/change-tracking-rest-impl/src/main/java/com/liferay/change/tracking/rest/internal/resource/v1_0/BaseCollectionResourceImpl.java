ba023dac633d3df21dbd79aa073577305db33e19
Máté Thurzó <mate.thurzo@liferay.com>
2019-06-06 12:55:14 +0200

    LPS-96370 Generated

--- modules/apps/change-tracking/change-tracking-rest-impl/src/main/java/com/liferay/change/tracking/rest/internal/resource/v1_0/BaseCollectionResourceImpl.java
+++ modules/apps/change-tracking/change-tracking-rest-impl/src/main/java/com/liferay/change/tracking/rest/internal/resource/v1_0/BaseCollectionResourceImpl.java
@@ -47,6 +47,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 /**
@@ -92,7 +93,7 @@ public abstract class BaseCollectionResourceImpl implements CollectionResource {
 		}
 	)
 	@Path("/collections")
-	@Produces({"application/json", "application/xml"})
+	@Produces({"application/json", "application/xml", "text/plain"})
 	@Tags(value = {@Tag(name = "Collection")})
 	public Collection postCollection(
 			@NotNull @Parameter(hidden = true) @QueryParam("companyId") Long
@@ -111,12 +112,16 @@ public abstract class BaseCollectionResourceImpl implements CollectionResource {
 		value = {@Parameter(in = ParameterIn.PATH, name = "collectionId")}
 	)
 	@Path("/collections/{collectionId}")
-	@Produces({"application/json", "text/plain"})
+	@Produces("text/plain")
 	@Tags(value = {@Tag(name = "Collection")})
-	public void deleteCollection(
+	public Response deleteCollection(
 			@NotNull @Parameter(hidden = true) @PathParam("collectionId") Long
 				collectionId)
 		throws Exception {
+
+		Response.ResponseBuilder responseBuilder = Response.ok();
+
+		return responseBuilder.build();
 	}
 
 	@Override
