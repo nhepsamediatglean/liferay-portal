8eda6fb0106575b50fefc16630c2e38a4ff9044d
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2019-12-10 18:37:08 -0300

    LPS-101799 Sort data record fields

--- modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/resource/v2_0/DataRecordResourceImpl.java
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/resource/v2_0/DataRecordResourceImpl.java
@@ -22,6 +22,7 @@ import com.liferay.data.engine.rest.internal.constants.DataActionKeys;
 import com.liferay.data.engine.rest.internal.dto.v2_0.util.DataDefinitionFieldUtil;
 import com.liferay.data.engine.rest.internal.dto.v2_0.util.DataDefinitionUtil;
 import com.liferay.data.engine.rest.internal.model.InternalDataRecordCollection;
+import com.liferay.data.engine.rest.internal.odata.entity.v1_0.DataRecordEntityModel;
 import com.liferay.data.engine.rest.internal.storage.DataRecordExporter;
 import com.liferay.data.engine.rest.internal.storage.DataStorageTracker;
 import com.liferay.data.engine.rest.resource.v2_0.DataRecordResource;
@@ -40,6 +41,10 @@ import com.liferay.dynamic.data.mapping.model.DDMStructure;
 import com.liferay.dynamic.data.mapping.model.DDMStructureVersion;
 import com.liferay.dynamic.data.mapping.service.DDMStorageLinkLocalService;
 import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
+import com.liferay.dynamic.data.mapping.util.DDMIndexer;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.Sort;
@@ -51,13 +56,19 @@ import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.odata.entity.EntityField;
+import com.liferay.portal.odata.entity.EntityModel;
+import com.liferay.portal.odata.entity.StringEntityField;
 import com.liferay.portal.vulcan.pagination.Page;
 import com.liferay.portal.vulcan.pagination.Pagination;
+import com.liferay.portal.vulcan.resource.EntityModelResource;
 import com.liferay.portal.vulcan.util.SearchUtil;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
@@ -66,6 +77,8 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.validation.ValidationException;
+
+import javax.ws.rs.core.MultivaluedMap;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -78,7 +91,8 @@ import org.osgi.service.component.annotations.ServiceScope;
 	properties = "OSGI-INF/liferay/rest/v2_0/data-record.properties",
 	scope = ServiceScope.PROTOTYPE, service = DataRecordResource.class
 )
-public class DataRecordResourceImpl extends BaseDataRecordResourceImpl {
+public class DataRecordResourceImpl
+	extends BaseDataRecordResourceImpl implements EntityModelResource {
 
 	@Override
 	public void deleteDataRecord(Long dataRecordId) throws Exception {
@@ -169,6 +183,9 @@ public class DataRecordResourceImpl extends BaseDataRecordResourceImpl {
 			PermissionThreadLocal.getPermissionChecker(),
 			dataRecordCollectionId, DataActionKeys.VIEW_DATA_RECORD);
 
+		DDLRecordSet ddlRecordSet = _ddlRecordSetLocalService.getDDLRecordSet(
+			dataRecordCollectionId);
+
 		return SearchUtil.search(
 			booleanQuery -> {
 			},
@@ -186,6 +203,46 @@ public class DataRecordResourceImpl extends BaseDataRecordResourceImpl {
 				_ddlRecordLocalService.getRecord(
 					GetterUtil.getLong(document.get(Field.ENTRY_CLASS_PK)))),
 			sorts);
+	}
+
+	@Override
+	public EntityModel getEntityModel(MultivaluedMap multivaluedMap)
+		throws PortalException {
+
+		Long dataDefinitionId = GetterUtil.getLong(
+			(String)multivaluedMap.getFirst("dataDefinitionId"));
+
+		if (Validator.isNull(dataDefinitionId)) {
+			long dataRecordCollectionId = GetterUtil.getLong(
+				(String)multivaluedMap.getFirst("dataRecordCollectionId"));
+
+			if (dataRecordCollectionId > 0) {
+				DDLRecordSet ddlRecordSet =
+					_ddlRecordSetLocalService.getDDLRecordSet(
+						dataRecordCollectionId);
+
+				DDMStructure ddmStructure = ddlRecordSet.getDDMStructure();
+
+				dataDefinitionId = ddmStructure.getStructureId();
+			}
+		}
+
+		List<EntityField> entityFields = new ArrayList<>();
+
+		if (dataDefinitionId > 0) {
+			DDMStructure ddmStructure =
+				_ddmStructureLocalService.getDDMStructure(dataDefinitionId);
+
+			for (String fieldName : ddmStructure.getFieldNames()) {
+				entityFields.add(
+					new StringEntityField(
+						fieldName,
+						locale -> _getSortableIndexFieldName(
+							ddmStructure.getStructureId(), fieldName, locale)));
+			}
+		}
+
+		return new DataRecordEntityModel(entityFields);
 	}
 
 	@Override
@@ -290,6 +347,9 @@ public class DataRecordResourceImpl extends BaseDataRecordResourceImpl {
 		_modelResourcePermission = modelResourcePermission;
 	}
 
+	@Reference
+	protected DDMIndexer ddmIndexer;
+
 	private DataStorage _getDataStorage(String dataStorageType) {
 		if (Validator.isNull(dataStorageType)) {
 			throw new ValidationException("Data storage type is null");
@@ -316,6 +376,20 @@ public class DataRecordResourceImpl extends BaseDataRecordResourceImpl {
 			ddmStructure.getGroupId(), ddmStructure.getStructureKey());
 
 		return ddlRecordSet.getRecordSetId();
+	}
+
+	private String _getSortableIndexFieldName(
+		long ddmStructureId, String fieldName, Locale locale) {
+
+		StringBundler sb = new StringBundler(
+			ddmIndexer.encodeName(ddmStructureId, fieldName, locale));
+
+		sb.append(StringPool.UNDERLINE);
+		sb.append("String");
+		sb.append(StringPool.UNDERLINE);
+		sb.append(Field.SORTABLE_FIELD_SUFFIX);
+
+		return sb.toString();
 	}
 
 	private DataRecord _toDataRecord(DDLRecord ddlRecord) throws Exception {
