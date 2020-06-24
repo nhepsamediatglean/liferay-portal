9e423052c8b578383ee0bd1027fd5d19fc68f74f
Brian Chan <brian.chan@liferay.com>
2020-06-24 10:38:31 -0700

    LPS-114779 all protected *ResourceTest methods should have @Override , Hugo sF

--- modules/apps/data-engine/data-engine-rest-test/src/testIntegration/java/com/liferay/data/engine/rest/resource/v2_0/test/DataDefinitionResourceTest.java
+++ modules/apps/data-engine/data-engine-rest-test/src/testIntegration/java/com/liferay/data/engine/rest/resource/v2_0/test/DataDefinitionResourceTest.java
@@ -59,7 +59,7 @@ public class DataDefinitionResourceTest
 		super.testDeleteDataDefinition();
 
 		DataDefinition dataDefinition =
-			testDeleteDataDefinition_addDataDefinition(
+			_testDeleteDataDefinition_addDataDefinition(
 				DataDefinition.toDTO(
 					DataDefinitionTestUtil.read("data-definition-parent.json")),
 				DataDefinition.toDTO(
@@ -552,7 +552,7 @@ public class DataDefinitionResourceTest
 			testGroup.getGroupId(), _CONTENT_TYPE, randomDataDefinition());
 	}
 
-	protected DataDefinition testDeleteDataDefinition_addDataDefinition(
+	private DataDefinition _testDeleteDataDefinition_addDataDefinition(
 			DataDefinition parentDataDefinition,
 			DataDefinition childDataDefinition)
 		throws Exception {
