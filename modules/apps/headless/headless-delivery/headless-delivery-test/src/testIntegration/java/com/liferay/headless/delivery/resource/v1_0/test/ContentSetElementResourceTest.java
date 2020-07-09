b926e514324043897e96ad4f6cb675e9430f82b1
Javier Gamarra <nhpatt@gmail.com>
2020-07-09 15:36:50 +0200

    LPS-114591 implement depot test, not added to rest builder because it is only used in these entities and will mean adding a new dependency in many test modules for nothing

--- modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/ContentSetElementResourceTest.java
+++ modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/ContentSetElementResourceTest.java
@@ -20,11 +20,21 @@ import com.liferay.asset.list.model.AssetListEntry;
 import com.liferay.asset.list.service.AssetListEntryLocalServiceUtil;
 import com.liferay.blogs.model.BlogsEntry;
 import com.liferay.blogs.service.BlogsEntryLocalServiceUtil;
+import com.liferay.depot.model.DepotEntry;
+import com.liferay.depot.service.DepotEntryLocalServiceUtil;
 import com.liferay.headless.delivery.client.dto.v1_0.ContentSetElement;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.test.util.JournalTestUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.search.test.util.SearchTestRule;
+
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -42,21 +52,80 @@ public class ContentSetElementResourceTest
 	public void setUp() throws Exception {
 		super.setUp();
 
-		_serviceContext = new ServiceContext();
-
-		_serviceContext.setScopeGroupId(testGroup.getGroupId());
-
-		long userId = TestPropsValues.getUserId();
-
-		_serviceContext.setUserId(userId);
+		_serviceContext = _getServiceContext(testGroup.getGroupId());
 
 		_assetListEntry = AssetListEntryLocalServiceUtil.addAssetListEntry(
-			userId, testGroup.getGroupId(), RandomTestUtil.randomString(),
+			TestPropsValues.getUserId(), testGroup.getGroupId(),
+			RandomTestUtil.randomString(),
 			AssetListEntryTypeConstants.TYPE_DYNAMIC, _serviceContext);
+
+		Map<Locale, String> nameMap = Collections.singletonMap(
+			LocaleUtil.getDefault(), RandomTestUtil.randomString());
+
+		_depotEntry = DepotEntryLocalServiceUtil.addDepotEntry(
+			nameMap, nameMap, _serviceContext);
+
+		ServiceContext depotServiceContext = _getServiceContext(
+			_depotEntry.getGroupId());
+
+		_depotAssetListEntry = AssetListEntryLocalServiceUtil.addAssetListEntry(
+			TestPropsValues.getUserId(), _depotEntry.getGroupId(),
+			RandomTestUtil.randomString(),
+			AssetListEntryTypeConstants.TYPE_DYNAMIC, depotServiceContext);
 	}
 
 	@Rule
 	public SearchTestRule searchTestRule = new SearchTestRule();
+
+	@Override
+	protected ContentSetElement
+			testGetAssetLibraryContentSetByKeyContentSetElementsPage_addContentSetElement(
+				Long assetLibraryId, String key,
+				ContentSetElement contentSetElement)
+		throws Exception {
+
+		return _toContentSetElement(
+			JournalTestUtil.addArticle(_depotAssetListEntry.getGroupId(), 0));
+	}
+
+	@Override
+	protected Long
+		testGetAssetLibraryContentSetByKeyContentSetElementsPage_getAssetLibraryId() {
+
+		return _depotEntry.getDepotEntryId();
+	}
+
+	@Override
+	protected String
+		testGetAssetLibraryContentSetByKeyContentSetElementsPage_getKey() {
+
+		return _depotAssetListEntry.getAssetListEntryKey();
+	}
+
+	@Override
+	protected ContentSetElement
+			testGetAssetLibraryContentSetByUuidContentSetElementsPage_addContentSetElement(
+				Long assetLibraryId, String uuid,
+				ContentSetElement contentSetElement)
+		throws Exception {
+
+		return _toContentSetElement(
+			JournalTestUtil.addArticle(_depotAssetListEntry.getGroupId(), 0));
+	}
+
+	@Override
+	protected Long
+		testGetAssetLibraryContentSetByUuidContentSetElementsPage_getAssetLibraryId() {
+
+		return _depotEntry.getDepotEntryId();
+	}
+
+	@Override
+	protected String
+		testGetAssetLibraryContentSetByUuidContentSetElementsPage_getUuid() {
+
+		return _depotAssetListEntry.getUuid();
+	}
 
 	@Override
 	protected ContentSetElement
@@ -108,6 +177,18 @@ public class ContentSetElementResourceTest
 			RandomTestUtil.randomString(), _serviceContext);
 	}
 
+	private ServiceContext _getServiceContext(long groupId) throws Exception {
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setAttribute(
+			WorkflowConstants.CONTEXT_USER_ID, TestPropsValues.getUserId());
+		serviceContext.setCompanyId(testGroup.getCompanyId());
+		serviceContext.setScopeGroupId(groupId);
+		serviceContext.setUserId(TestPropsValues.getUserId());
+
+		return serviceContext;
+	}
+
 	private ContentSetElement _toContentSetElement(BlogsEntry blogsEntry) {
 		return new ContentSetElement() {
 			{
@@ -117,7 +198,20 @@ public class ContentSetElementResourceTest
 		};
 	}
 
+	private ContentSetElement _toContentSetElement(
+		JournalArticle journalArticle) {
+
+		return new ContentSetElement() {
+			{
+				id = journalArticle.getId();
+				title = journalArticle.getTitle();
+			}
+		};
+	}
+
 	private AssetListEntry _assetListEntry;
+	private AssetListEntry _depotAssetListEntry;
+	private DepotEntry _depotEntry;
 	private ServiceContext _serviceContext;
 
 }
