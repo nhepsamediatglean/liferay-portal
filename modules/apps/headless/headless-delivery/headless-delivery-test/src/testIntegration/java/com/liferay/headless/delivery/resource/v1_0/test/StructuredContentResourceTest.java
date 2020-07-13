02251e6bfe2ff46befceea5171dfe5543069a9cc
Brian Chan <brian.chan@liferay.com>
2020-07-13 13:08:38 -0700

    LPS-114591 Inline as many of these one time uses of ServiceContext

--- modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/StructuredContentResourceTest.java
+++ modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/StructuredContentResourceTest.java
@@ -91,15 +91,16 @@ public class StructuredContentResourceTest
 	public void setUp() throws Exception {
 		super.setUp();
 
-		ServiceContext serviceContext = new ServiceContext();
-
-		serviceContext.setCompanyId(testGroup.getCompanyId());
-		serviceContext.setUserId(TestPropsValues.getUserId());
-
 		_depotEntry = DepotEntryLocalServiceUtil.addDepotEntry(
 			Collections.singletonMap(
 				LocaleUtil.getDefault(), RandomTestUtil.randomString()),
-			null, serviceContext);
+			null,
+			new ServiceContext() {
+				{
+					setCompanyId(testGroup.getCompanyId());
+					setUserId(TestPropsValues.getUserId());
+				}
+			});
 
 		testGroup = _depotEntry.getGroup();
 
