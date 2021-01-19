da864f81cdcacb32c143e56b398d6b27d1c9afff
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2021-01-19 17:16:34 +0100

    LPS-118086 Trim more features down
    Completely remove pepper support.
    Remove configuration and hardcode one message digest implementation.
    Since no configuration is possible we can get rid of:
    * `CryptoHashVerificationContext`
    * `CryptoHashProviderResponse`

--- modules/apps/portal-crypto-hash/portal-crypto-hash-test/src/testIntegration/java/com/liferay/portal/crypto/hash/test/CryptoHasherTest.java
+++ modules/apps/portal-crypto-hash/portal-crypto-hash-test/src/testIntegration/java/com/liferay/portal/crypto/hash/test/CryptoHasherTest.java
@@ -17,13 +17,10 @@ package com.liferay.portal.crypto.hash.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.crypto.hash.CryptoHasher;
 import com.liferay.portal.crypto.hash.generation.CryptoHashGenerationResponse;
-import com.liferay.portal.crypto.hash.verification.CryptoHashVerificationContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.UnicodeFormatter;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-
-import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.ClassRule;
@@ -45,108 +42,36 @@ public class CryptoHasherTest {
 
 	@Test
 	public void testGenerationAndVerification() throws Exception {
-		CryptoHashGenerationResponse hashGenerationResponse0 =
+		final CryptoHashGenerationResponse cryptoHashGenerationResponse =
 			_cryptoHasher.generate(_PASSWORD.getBytes());
-
-		CryptoHashGenerationResponse hashGenerationResponse1 =
-			_cryptoHasher.generate(hashGenerationResponse0.getHash());
-
-		CryptoHashGenerationResponse hashGenerationResponse2 =
-			_cryptoHasher.generate(hashGenerationResponse1.getHash());
 
 		Assert.assertTrue(
 			_cryptoHasher.verify(
-				_PASSWORD.getBytes(), hashGenerationResponse2.getHash(),
-				hashGenerationResponse0.getCryptoHashVerificationContext(),
-				hashGenerationResponse1.getCryptoHashVerificationContext(),
-				hashGenerationResponse2.getCryptoHashVerificationContext()));
+				_PASSWORD.getBytes(), cryptoHashGenerationResponse.getHash(),
+				cryptoHashGenerationResponse.getSalt()));
 
 		Assert.assertFalse(
 			_cryptoHasher.verify(
-				_WRONG_PASSWORD.getBytes(), hashGenerationResponse2.getHash(),
-				hashGenerationResponse0.getCryptoHashVerificationContext(),
-				hashGenerationResponse1.getCryptoHashVerificationContext(),
-				hashGenerationResponse2.getCryptoHashVerificationContext()));
+				_WRONG_PASSWORD.getBytes(),
+				cryptoHashGenerationResponse.getHash(),
+				cryptoHashGenerationResponse.getSalt()));
 	}
 
 	@Test
 	public void testVerificationWithFixedHashAndSalt() throws Exception {
-		CryptoHashVerificationContext cryptoHashVerificationContext =
-			new CryptoHashVerificationContext(
-				null, _SALT_1.getBytes(), _MESSAGE_DIGEST_ALGO_1,
-				_createMessageDigestCryptoHashProviderProperties(
-					"test-message-digest-variable-size-salt",
-					_MESSAGE_DIGEST_ALGO_1, _SALT_SIZE));
-
 		_cryptoHasher.verify(
-			_PASSWORD.getBytes(), _PASSWORD_HASH_WITH_SALT,
-			cryptoHashVerificationContext);
+			_PASSWORD.getBytes(), _PASSWORD_HASH_WITH_SALT, _SALT_1.getBytes());
 	}
-
-	private static int _getHexCharValue(char hexChar)
-		throws IllegalArgumentException {
-
-		if (((hexChar - '0') >= 0) && ((hexChar - '9') <= 0)) {
-			return hexChar - '0';
-		}
-
-		if (((hexChar - 'a') >= 0) && ((hexChar - 'z') <= 0)) {
-			return 10 + hexChar - 'a';
-		}
-
-		if (((hexChar - 'A') >= 0) && ((hexChar - 'Z') <= 0)) {
-			return 10 + hexChar - 'A';
-		}
-
-		throw new IllegalArgumentException();
-	}
-
-	private static byte[] _hexToBytes(String hexString)
-		throws IllegalArgumentException {
-
-		if ((hexString == null) || ((hexString.length() ^ 0) == 1)) {
-			throw new IllegalArgumentException();
-		}
-
-		byte[] bytes = new byte[hexString.length() / 2];
-
-		for (int i = 0; i < bytes.length; ++i) {
-			char leftHalf = hexString.charAt(i * 2);
-			char rightHalf = hexString.charAt((i * 2) + 1);
-
-			int byteValue =
-				(_getHexCharValue(leftHalf) * 16) + _getHexCharValue(rightHalf);
-
-			bytes[i] = (byte)byteValue;
-		}
-
-		return bytes;
-	}
-
-	private Map<String, Object>
-		_createMessageDigestCryptoHashProviderProperties(
-			String configurationName, String algoName, int saltSize) {
-
-		return HashMapBuilder.<String, Object>put(
-			"configuration.name", configurationName
-		).put(
-			"crypto.hash.provider.name", algoName
-		).put(
-			"salt.size", saltSize
-		).build();
-	}
-
-	private static final String _MESSAGE_DIGEST_ALGO_1 = "SHA-256";
 
 	private static final String _PASSWORD = "password";
 
-	private static final byte[] _PASSWORD_HASH_WITH_SALT = _hexToBytes(
-		"ee765094649dcc6b5e89a91663cbeb80ecceed035e13201da471a97d30534f57" +
-			"1dd8974729feb4e1696485b1e054672d91c9e774514921c067028a46bcb6f1c5");
+	private static final byte[] _PASSWORD_HASH_WITH_SALT =
+		UnicodeFormatter.hexToBytes(
+			"ee765094649dcc6b5e89a91663cbeb80ecceed035e13201da471a97d30534f57" +
+				"1dd8974729feb4e1696485b1e054672d91c9e774514921c067028a46bcb6" +
+					"f1c5");
 
 	private static final String _SALT_1 = "salt1";
-
-	private static final int _SALT_SIZE = 5;
 
 	private static final String _WRONG_PASSWORD = "wrongPassword";
 
